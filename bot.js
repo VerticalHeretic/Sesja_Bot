5976b050bd16f7e4e59cc3ea089b1b41c1157af7
LSWarss <ls.warss98@gmail.com>
2021-01-24 18:21:28 +0100

    Adding points json for testing

--- bot.js
+++ bot.js
@@ -2,17 +2,25 @@ console.log('Beep beep 🦾 ')
 
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
+const fs = require('fs')
 
-client.on('ready', () => {
-    console.log('💙');
-});
 
+// VARIABLES
+ 
+let points = JSON.parse(fs.readFileSync("./points.json", "utf-8"));
+
+// CONSTANTS 
+const 
 const replies = [
     "The Force will be with you. Always. ✨ ",
     "“I find your lack of faith disturbing. 🤖 ",
     "Now, young Skywalker, you will die. ⚡️ ", 
     "It's no use Anakin I have the high ground 🏔"
 ]
+
+client.on('ready', () => {
+    console.log('💙');
+});
 
 client.on('message', msg => {
     console.log(msg.content);
@@ -22,5 +30,10 @@ client.on('message', msg => {
     }
 });
 
+client.on('message',  msg => {
+    if (msg.channel.id == '802931490019082300' && msg.content === 'Points') {
+        console.log(points)
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
