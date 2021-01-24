8151e174e588449302aca9190632359720bc77bf
LSWarss <ls.warss98@gmail.com>
2021-01-24 21:28:05 +0100

    Tried saving to the json the points, but will need to migrate to sql for easyer saving

--- bot.js
+++ bot.js
@@ -5,18 +5,16 @@ const client = new Client();
 const fs = require('fs')
 
 
-// VARIABLES
- 
-let points = JSON.parse(fs.readFileSync("./points.json", "utf-8"));
-
 // CONSTANTS 
+let data = require('./data/mainData.json');
+
 
 const replies = [
     "The Force will be with you. Always. ✨ ",
     "“I find your lack of faith disturbing. 🤖 ",
     "Now, young Skywalker, you will die. ⚡️ ", 
     "It's no use Anakin I have the high ground 🏔"
-]
+];
 
 client.on('ready', () => {
     console.log('💙');
@@ -28,12 +26,25 @@ client.on('message', msg => {
         const index = Math.floor(Math.random() * replies.length);
         msg.channel.send(replies[index]);
     }
-});
-
-client.on('message',  msg => {
     if (msg.channel.id == '802931490019082300' && msg.content === 'Points') {
-        console.log(points)
+        for (i=0; i < data.points.length; i++) {
+            console.log(data.points[i].username)
+            if (msg.author.username == data.points[i].username) {
+                msg.reply(`You have ${data.points[i].number} point right now`);
+            }
+            if (msg.author.username != data.points[i].username) {
+                msg.reply("You are not in database yet, adding you right now..")
+                data.points.push({"username": msg.author.username, "number": 0})
+                console.log(data);
+                fs.writeFile('./data/mainData.json', JSON.stringify(data), function(err, result){
+                    if(err) console.log('error', err)
+                });
+                break
+            }
+        }
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+
+client.login("process.env.BOT_TOKEN");
+
