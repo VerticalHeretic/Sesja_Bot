aacb0587a4c0896570875f8fd6f00dab9327ece4
LSWarss <ls.warss98@gmail.com>
2021-01-25 22:48:45 +0100

    Created sesja database table implementation

--- bot.js
+++ bot.js
@@ -15,22 +15,13 @@ const replies = [
 
 client.on('ready', () => {
     console.log('💙');
-    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'points';").get();
-    if(!table['count(*)']) {
-        // If the table isn't there, create it and setup the database correctly.
-        sql.prepare("CREATE TABLE points (id TEXT PRIMARY KEY, user TEXT, points INTEGER);").run();
-        sql.prepare("CREATE UNIQUE INDEX idx_points_id ON points (id);").run();
-        sql.pragma("synchronous = 1");
-        sql.pragma("journal_mode = wal");
-    }
-    client.getPoints = sql.prepare("SELECT * FROM points WHERE user = ?")
-    client.setPoints = sql.prepare("INSERT OR REPLACE INTO points (id, user, points) VALUES (@id,@user,@points)")
+    createPointsTable();
+    createSesjaTable();
 });
 
 client.on('message', msg => {
     if (msg.author.bot) return;
     let score;
-    console.log(msg.content);
     if( msg.channel.id == '802931490019082300' && msg.content === 'Force') {
         const index = Math.floor(Math.random() * replies.length);
         msg.channel.send(replies[index]);
@@ -47,6 +38,32 @@ client.on('message', msg => {
         return msg.reply(`You currently have ${score.points} 🤘🏻`)
     }
 });
+
+function createPointsTable() {
+    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'points';").get();
+    if(!table['count(*)']) {
+        // If the table isn't there, create it and setup the database correctly.
+        sql.prepare("CREATE TABLE points (id TEXT PRIMARY KEY, user TEXT, points INTEGER);").run();
+        sql.prepare("CREATE UNIQUE INDEX idx_points_id ON points (id);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
+    }
+    client.getPoints = sql.prepare("SELECT * FROM points WHERE user = ?");
+    client.setPoints = sql.prepare("INSERT OR REPLACE INTO points (id, user, points) VALUES (@id,@user,@points)");
+}
+
+function createSesjaTable() {
+    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'sesja';").get();
+    if(!table['count(*)']) {
+        sql.prepare("CREATE TABLE sesja (id INTEGER AUTOINCREMENT, subject TEXT, when TEXT, professor TEXT);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
+    }
+    client.getSesja = sql.prepare("SELECT * FROM sesja");
+    client.setSesja = sql.prepare("INSERT OR REPLACE INTO sesja (subject, when, professor) VALUES (@subject, @when, @professor");
+    client.getSesjaForSubject = sql.prepare("SELECT * FROM sesja WHERE subject = ?");
+    client.getSesjaForProfessor = sql.prepare("SELECT * FROM sesja WHERE professor = ?")
+}
 
 
 client.login(process.env.BOT_TOKEN);
